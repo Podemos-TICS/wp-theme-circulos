0ec0856eab915f5003c1fb5f0daea96c4d71edb7
Óscar Bustos <elchiconube@MacBook-Air-de-Oscar.local>
2014-08-23 18:55:59 +0200

    footer corregido

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -4,9 +4,10 @@
 		<aside class="contact_info">
 			<address itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 				<span class="icon location" itemprop="streetAddress"><?php echo get_option( 'direccion' ); ?></span>
-				<p><span itemprop="postalCode"><?php echo get_option( 'codigo_postal' ); ?></span><span itemprop="addressLocality">, <?php echo get_option( 'localidad' ); ?></span><span itemprop="addressRegion">, <?php echo get_option( 'provincia' ); ?></span></p>
-				<span itemprop="addressCountry"><?php echo get_option( 'pais' ); ?></span>
+				<p><span itemprop="addressLocality"><?php echo get_option( 'localidad' ); ?></span><span itemprop="addressRegion">, <?php echo get_option( 'provincia' ); ?></span></p>
+				<p><span itemprop="postalCode"><?php echo get_option( 'codigo_postal' ); ?></span> - <span itemprop="addressCountry"><?php echo get_option( 'pais' ); ?></span></p>
 				<a class="icon mail" href="mailto:<?php bloginfo( 'admin_email' ); ?>" itemprop="email"><?php bloginfo( 'admin_email' ); ?></a>
+				<a class="icon url" href="/contactar" itemprop="email">Contactar</a>
 				<!--<span class="icon phone" itemprop="telephone"><?php echo get_option( 'telefono' ); ?></span>-->
 			</address>
 		</aside>
